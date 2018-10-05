e9565a7c7a13b9bb614c02a8795dc291bc31010d
sewonist <sewonist@gmail.com>
2018-10-04 20:49:52 -0700

    Fix

--- msg.js
+++ msg.js
@@ -1,6 +1,3 @@
-var accountSid = 'AC68e7a38ef26935a0befb587758f993b9'; // Your Account SID from www.twilio.com/console
-var authToken = '2214eaaffb8dbe1d4e94e29801e61c32';   // Your Auth Token from www.twilio.com/console
-
 var config = require('./config');
 var twilio = require('twilio');
 var client = new twilio(config.accountSid, config.authToken);
